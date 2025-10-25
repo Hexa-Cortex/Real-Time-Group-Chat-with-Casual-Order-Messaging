efe7b70e0af7c0c1d9ef079a38e75194d0b6cfd8
Hexa-Cortex <gowthams0959@gmail.com>
2025-10-25 15:13:55 +0530

    constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+export const USER_NAMES = ['Alice', 'Bob', 'Charlie', 'Diana', 'Eve'];
+
+export const USER_COLORS = [
+  'bg-blue-100 border-blue-300',
+  'bg-green-100 border-green-300',
+  'bg-purple-100 border-purple-300',
+  'bg-orange-100 border-orange-300',
+  'bg-pink-100 border-pink-300'
+];
+
+export const DELAY_OPTIONS = {
+  FAST: 500,
+  NORMAL: 1000,
+  SLOW: 2000
+};
+
+export const DEFAULT_NUM_USERS = 3;
+export const MIN_USERS = 2;
+export const MAX_USERS = 5;
